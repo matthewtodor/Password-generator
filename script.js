a04d4079ef0ad47bb7bd68ba7f08913d16e064fd
Matthew Todor <unclejunglegeorge@gmail.com>
2022-02-11 10:11:48 -0700

    Added comments in index.html and script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // "\\" is an escape character, functions funny in js
 
-//add arrays with available character options
+//arrays with available character options
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = [
 	"@",
@@ -94,26 +94,35 @@ function getUserInput() {
 	var passwordLength = prompt(
 		"How long should your password be? Please pick between 8 and 128 characters"
 	);
+	//if less than 8 characters,
 	if (passwordLength < 8) {
+		//alert will display
 		alert("Password length must be at least 8 characters");
 		return;
 	}
+	//if more than 128 characters
 	if (passwordLength > 128) {
+		//alert will display
 		alert("Password length must be no more than 128 characters");
 		return;
 	}
+	//asks if lowercase letters should be included in var characterpool. if "ok", boolean is true
 	var confirmLowercase = confirm(
 		"do you want your password to have lowercase characters?"
 	);
+	//asks if uppercase letters should be included in var characterpool. if "ok", boolean is true
 	var confirmUppercase = confirm(
 		"do you want your password to have uppercase characters?"
 	);
+	//asks if special characters should be included in var characterpool. if "ok", boolean is true
 	var confirmSpecial = confirm(
 		"do you want your password to have special characters?"
 	);
+	//asks if numbers should be included in var characterpool. if "ok", boolean is true
 	var confirmNumber = confirm(
 		"do you want your password to have numeric characters?"
 	);
+	//sets resulting boolean value as a value in another object for reference
 	var userChoice = {
 		// object = {key:value}
 		userPwLength: passwordLength,
@@ -122,17 +131,22 @@ function getUserInput() {
 		userSpecial: confirmSpecial,
 		userNumber: confirmNumber,
 	};
-
+	//returns the resulting object to global scope for use in generatePassword function
 	return userChoice;
 }
+//function to randomize an array, to be used in generatePassword funtion, randomizing characterPool array
 function randomizeArray(array) {
 	var index = Math.floor(Math.random() * array.length);
 	var indexValue = array[index];
 	return indexValue;
 }
+//function that generates the password, returning the final password to global scope
 function generatePassword() {
+	//sets userPrompt var in generatePassword as getUserInput function from above
 	var userPrompt = getUserInput();
+	//the final password
 	var finalPassword = [];
+	//the array that holds all available characters for 
 	var characterPool = [];
 	if (userPrompt.userLowercase === true) {
 		characterPool = characterPool.concat(lowerCase);
@@ -160,6 +174,7 @@ function generatePassword() {
 		var stagedArray = randomizeArray(characterPool);
 		finalPassword.push(stagedArray);
 	}
+	//returns object to global scope for 
 	return finalPassword.join("");
 }
 
